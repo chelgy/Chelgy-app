49a825bcb33f08f513b449bbdaecce768af8d163
baybae <chelsea@baybaes-MacBook-Pro.local>
2026-06-26 15:13:17 -0400

    add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import ChelgyApp from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import ChelgyApp from "./App.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChelgyApp />
-  </StrictMode>,
-)
+  </StrictMode>
+);
